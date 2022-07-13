10a0940dfc0736877e61d94092637efca3cbb352
german gatica <gergati1994@gmail.com>
2022-07-12 21:16:31 -0300

    Ejercicios js

--- clase_02/index.js
+++ clase_02/index.js
@@ -1,14 +1,13 @@
-const cosas = [{
-    name: "German",
-    edad: 28,
-    isDeveloper: true,
-    date: 29-04-1994,
-    libro: {
-        libro: "El psicoanalista",
+const cosas = [
+    "German",
+    28,
+    true,
+    29-04-1994,
+    {   libro: "El psicoanalista",
         autor: "John Katzenbach",
         fecha: 12-07-2022,
         url: "www.google.com",
     }
-}]
+]
 
 console.log(cosas)
